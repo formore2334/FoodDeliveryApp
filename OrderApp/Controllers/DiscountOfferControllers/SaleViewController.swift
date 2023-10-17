5cddb1aa4960c4f38de958ed8adc7976f31fc3b2
Dzmitry Kalbasin <myattention15@gmail.com>
2023-10-17 20:13:38 +0500

    Separated mockData's of menu from VC to model extension

--- OrderApp/Controllers/DiscountOfferControllers/SaleViewController.swift
+++ OrderApp/Controllers/DiscountOfferControllers/SaleViewController.swift
@@ -12,50 +12,7 @@ class SaleViewController: UIViewController {
 private var titleLabel = UILabel()
 private var collectionView: UICollectionView?
 
-private let models: [Menu] = [
-    Menu(imageName: "cube", title: "Combo", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Combo 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Combo 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Combo 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Combo 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Combo 5", description: "Good Choise to..")
-        ]),
-    Menu(imageName: "cube", title: "Burgers", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Burger 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Burger 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Burger 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Burger 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Burger 5", description: "Good Choise to..")
-        ]),
-    Menu(imageName: "cube", title: "Fries", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Fries 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Fries 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Fries 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Fries 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Fries 5", description: "Good Choise to..")
-        ]),
-    Menu(imageName: "cube", title: "Drinks", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Drinks 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Drinks 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Drinks 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Drinks 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Drinks 5", description: "Good Choise to..")
-        ]),
-    Menu(imageName: "cube", title: "Souses", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Souses 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Souses 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Souses 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Souses 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Souses 5", description: "Good Choise to..")
-        ]),
-    Menu(imageName: "cube", title: "Chicken", description: "Nice food", menuItem: [
-        MenuItem(imageName: "cube", title: "Chicken 1", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Chicken 2", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Chicken 3", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Chicken 4", description: "Good Choise to.."),
-        MenuItem(imageName: "cube", title: "Chicken 5", description: "Good Choise to..")
-        ])
-]
+    private let menu: [Menu] = Menu.mockData
 
 override func viewDidLoad() {
     super.viewDidLoad()
--- OrderApp/Model/MenuModel.swift
+++ OrderApp/Model/MenuModel.swift
@@ -21,4 +21,49 @@ struct Menu {
     let menuItem: [MenuItem]
 }
 
-
+extension Menu {
+    static let mockData: [Menu] = [
+        Menu(imageName: "cube", title: "Combo", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Combo 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Combo 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Combo 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Combo 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Combo 5", description: "Good Choise to..")
+            ]),
+        Menu(imageName: "cube", title: "Burgers", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Burger 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Burger 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Burger 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Burger 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Burger 5", description: "Good Choise to..")
+            ]),
+        Menu(imageName: "cube", title: "Fries", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Fries 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Fries 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Fries 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Fries 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Fries 5", description: "Good Choise to..")
+            ]),
+        Menu(imageName: "cube", title: "Drinks", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Drinks 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Drinks 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Drinks 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Drinks 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Drinks 5", description: "Good Choise to..")
+            ]),
+        Menu(imageName: "cube", title: "Souses", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Souses 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Souses 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Souses 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Souses 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Souses 5", description: "Good Choise to..")
+            ]),
+        Menu(imageName: "cube", title: "Chicken", description: "Nice food", menuItem: [
+            MenuItem(imageName: "cube", title: "Chicken 1", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Chicken 2", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Chicken 3", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Chicken 4", description: "Good Choise to.."),
+            MenuItem(imageName: "cube", title: "Chicken 5", description: "Good Choise to..")
+            ])
+    ]
+}
