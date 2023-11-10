ed54bff8506a8d0026654a5db4af08a31c210284
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-10 20:49:11 +0500

    Did change appearance & layout to detailMenuVC

--- OrderApp/Controllers/DetailMenuControllers/CategoryDetailMenuViewController.swift
+++ OrderApp/Controllers/DetailMenuControllers/CategoryDetailMenuViewController.swift
@@ -9,20 +9,29 @@ import UIKit
 
 // MARK: - Detail menu for each category from menu
 
-class CategoryDetailMenuViewController: UIViewController {
+class CategoryDetailMenuViewController: UICollectionViewController {
 
-    var menu: Menu
-    
     var coordinator: MainCoordinator?
     
-    private var titleLabel = UILabel()
+    var menu: Menu
     
-    private var collectionView: UICollectionView?
+    private let logoImageView = UIImageView()
+    
+    private let logoContainerView = UIView()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        //label.text = "Top Salers"
+        label.font = UIFont.boldSystemFont(ofSize: 25)
+        label.textColor = .black
+        label.textAlignment = .left
+        return label
+    }()
     
     init(menu: Menu, coordinator: MainCoordinator? = nil) {
         self.menu = menu
         self.coordinator = coordinator
-        super.init(nibName: nil, bundle: nil)
+        super.init(collectionViewLayout: UICollectionViewLayout())
     }
     
     required init?(coder: NSCoder) {
@@ -33,115 +42,122 @@ class CategoryDetailMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configureTitleLabel()
+        configureLogoImage()
         configureVC()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.tintColor = .black
             UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .normal)
     }
 
    //MARK: - Configure CollectionView
     
     func configureVC() {
-        
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layoutCollection())
-        
-        guard let collectionView = collectionView else { return }
-        view.addSubview(collectionView)
-        
-        setConstraints()
-        
-        collectionView.backgroundColor = .white
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        view.addSubview(titleLabel)
+        titleLabel.text = menu.title
         
         collectionView.register(DetailMenuCollectionViewCell.self, forCellWithReuseIdentifier: DetailMenuCollectionViewCell.identifier)
-        collectionView.register(DetailMenuReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailMenuReusableView.idintifier)
+        collectionView.collectionViewLayout = layoutCollection()
+    
+        setBackgroundImage()
+        setConstraints()
     }
     
-    
-    func layoutCollection() -> UICollectionViewFlowLayout {
-        let layout = UICollectionViewFlowLayout()
-        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-        layout.minimumLineSpacing = 20
-        layout.sectionHeadersPinToVisibleBounds = true
-        
-        return layout
+    func setBackgroundImage() {
+        let backgroundImageView = UIImageView(image: UIImage(named: "backgroundImageSecondPart"))
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.clipsToBounds = true
+        backgroundImageView.layer.opacity = 0.1
+
+        collectionView.backgroundView = backgroundImageView
     }
+
     
     //MARK: - App Logo
     
-    func configureTitleLabel() {
+    func configureLogoImage() {
+        logoImageView.image = UIImage(named: "logoWelms")
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.clipsToBounds = true
+        logoImageView.layer.masksToBounds = true
+        logoImageView.backgroundColor = .systemGray6
         
-        view.addSubview(titleLabel)
-        titleLabel.text = "Welm's"
-        titleLabel.textColor = .white
-        titleLabel.frame = CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 90)
-        titleLabel.backgroundColor = UIColor(named: "redOrange")
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 25)
-        titleLabel.textAlignment = .center
-        titleLabel.numberOfLines = 0
-        titleLabel.adjustsFontSizeToFitWidth = true
-    
+        view.addSubview(logoContainerView)
+        logoContainerView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height / 7)
+        logoContainerView.backgroundColor = .systemGray6
+        logoContainerView.addSubview(logoImageView)
+       
+        setLogoImageConstraints()
     }
+
     
     //MARK: - Constraints
-    func setConstraints() {
-        guard let collectionView = collectionView else { return }
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
+      
+    func setLogoImageConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+         logoImageView.heightAnchor.constraint(equalToConstant: 60),
+         logoImageView.widthAnchor.constraint(equalToConstant: 200),
+
+         logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -40),
+         logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
+    
+      func setConstraints() {
+          titleLabel.translatesAutoresizingMaskIntoConstraints = false
+          collectionView.translatesAutoresizingMaskIntoConstraints = false
+          
+          NSLayoutConstraint.activate([
+              titleLabel.topAnchor.constraint(equalTo: logoContainerView.bottomAnchor, constant: 20),
+              titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+             
+              collectionView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: -10),
+              collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+              collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+              collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+              
+          ])
+      }
 
 }
 
-//MARK: - Delegat's
-
-extension CategoryDetailMenuViewController: UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let detailInfoVC = DetailInfoViewController(menuItem: menu.menuItem[indexPath.row], coordinator: coordinator)
-        
-        navigationController?.pushViewController(detailInfoVC, animated: true)
-    }
-    
-}
-
-extension CategoryDetailMenuViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension CategoryDetailMenuViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return menu.menuItem.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailMenuCollectionViewCell.identifier, for: indexPath) as! DetailMenuCollectionViewCell
         
         cell.configure(name: menu.menuItem[indexPath.row].imageName, title: menu.menuItem[indexPath.row].title)
-        
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DetailMenuReusableView.idintifier, for: indexPath) as! DetailMenuReusableView
-
-        header.configure(title: menu.title)
-        return header
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailInfoVC = DetailInfoViewController(menuItem: menu.menuItem[indexPath.row], coordinator: coordinator)
+        
+        navigationController?.pushViewController(detailInfoVC, animated: true)
     }
+  
 }
 
 extension CategoryDetailMenuViewController: UICollectionViewDelegateFlowLayout {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: view.bounds.size.width, height: 50)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    
+    func layoutCollection() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        layout.minimumLineSpacing = 50
+        
+        return layout
     }
     
 }
--- OrderApp/Controllers/DetailMenuControllers/DetailMenuCollectionViewCell.swift
+++ OrderApp/Controllers/DetailMenuControllers/DetailMenuCollectionViewCell.swift
@@ -11,14 +11,23 @@ class DetailMenuCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "DetailMenuCollectionViewCell"
     
+    private var cellContainer: UIView = {
+        let container = UIView()
+        container.backgroundColor = .systemGray5
+        container.layer.cornerRadius = 10
+        container.layer.cornerRadius = 100.0 / 2.0
+        container.layer.borderWidth = 1
+        container.layer.borderColor = UIColor.black.cgColor
+        container.layer.opacity = 0.5
+        return container
+    }()
+    
     private let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
         imageView.layer.masksToBounds = true
-        imageView.layer.cornerRadius = 100.0 / 2.0
-        imageView.layer.borderWidth = 2
-        imageView.layer.borderColor = UIColor(named: "redOrange")?.cgColor
+        imageView.layer.cornerRadius = 90.0 / 2.0
         return imageView
     }()
     
@@ -31,24 +40,16 @@ class DetailMenuCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super .init(frame: frame)
 
-        configureDetailMenu()
+        configureCell()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        
-        imageView.image = nil
-        titleLabel.text = nil
-    }
-    
-    //MARK: - Configure DetailMenuView
-    
-    func configureDetailMenu() {
+
+
+    func configureCell() {
+        contentView.addSubview(cellContainer)
         contentView.addSubview(imageView)
         contentView.addSubview(titleLabel)
         
@@ -63,21 +64,24 @@ class DetailMenuCollectionViewCell: UICollectionViewCell {
     //MARK: - Constraints
     
     func setDetailMenuConstraints() {
+        cellContainer.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            imageView.heightAnchor.constraint(equalToConstant: 100),
-            imageView.widthAnchor.constraint(equalToConstant: 100),
+            cellContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            cellContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            cellContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.centerXAnchor.constraint(equalTo: cellContainer.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: cellContainer.centerYAnchor),
+            imageView.heightAnchor.constraint(equalTo: cellContainer.heightAnchor, multiplier: 0.9),
+            imageView.widthAnchor.constraint(equalTo: cellContainer.widthAnchor, multiplier: 0.9),
             
-            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            titleLabel.topAnchor.constraint(equalTo: cellContainer.bottomAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: cellContainer.centerXAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 20)
         ])
     }
     
