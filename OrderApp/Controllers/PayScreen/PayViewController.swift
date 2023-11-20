8547c1ab3563b9b2a291647f759601a71ca77405
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-20 19:16:15 +0500

    Implement pay button & vc

---
+++ OrderApp/Controllers/PayScreen/PayViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PayViewController.swift
+//  OrderApp
+//
+//  Created by ForMore on 17/11/2023.
+//
+
+import UIKit
+
+class PayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Pay View"
+    }
+
+}
