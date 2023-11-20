2706f8af0ea6dd0425a2096eb8a6f6784ecdb3e5
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-20 19:14:18 +0500

    Did change image name in models

--- OrderApp/Model/Menu.swift
+++ OrderApp/Model/Menu.swift
@@ -24,47 +24,40 @@ struct Menu {
 
 extension Menu {
     static let mockData: [Menu] = [
-        Menu(imageName: "cube", title: "Combo", description: "Nice food", menuItem: [
-            MenuItem(price: 15.99, imageName: "cube", title: "Combo 1", description: "Good Choise to.."),
-            MenuItem(price: 16.99, imageName: "cube", title: "Combo 2", description: "Good Choise to.."),
-            MenuItem(price: 20.99, imageName: "cube", title: "Combo 3", description: "Good Choise to.."),
-            MenuItem(price: 13.99, imageName: "cube", title: "Combo 4", description: "Good Choise to.."),
-            MenuItem(price: 19.99, imageName: "cube", title: "Combo 5", description: "Good Choise to..")
+        Menu(imageName: "comboLogo", title: "Combo", description: "Nice food", menuItem: [
+            MenuItem(price: 15.99, imageName: "combo1", title: "Combo 1", description: "Good Choise to.."),
+            MenuItem(price: 16.99, imageName: "combo2", title: "Combo 2", description: "Good Choise to.."),
+            MenuItem(price: 20.99, imageName: "combo3", title: "Combo 3", description: "Good Choise to.."),
+            MenuItem(price: 13.99, imageName: "combo4", title: "Combo 4", description: "Good Choise to.."),
+            MenuItem(price: 19.99, imageName: "combo5", title: "Combo 5", description: "Good Choise to..")
             ]),
-        Menu(imageName: "cube", title: "Burgers", description: "Nice food", menuItem: [
-            MenuItem(price: 11.99, imageName: "cube", title: "Burger 1", description: "Good Choise to.."),
-            MenuItem(price: 9.99, imageName: "cube", title: "Burger 2", description: "Good Choise to.."),
-            MenuItem(price: 13.99, imageName: "cube", title: "Burger 3", description: "Good Choise to.."),
-            MenuItem(price: 11.99, imageName: "cube", title: "Burger 4", description: "Good Choise to.."),
-            MenuItem(price: 10.99, imageName: "cube", title: "Burger 5", description: "Good Choise to..")
+        Menu(imageName: "burgerLogo", title: "Burgers", description: "Nice food", menuItem: [
+            MenuItem(price: 11.99, imageName: "burger1", title: "Burger 1", description: "Good Choise to.."),
+            MenuItem(price: 9.99, imageName: "burger2", title: "Burger 2", description: "Good Choise to.."),
+            MenuItem(price: 13.99, imageName: "burger3", title: "Burger 3", description: "Good Choise to.."),
+            MenuItem(price: 11.99, imageName: "burger4", title: "Burger 4", description: "Good Choise to.."),
+            MenuItem(price: 10.99, imageName: "burger5", title: "Burger 5", description: "Good Choise to..")
             ]),
-        Menu(imageName: "cube", title: "Fries", description: "Nice food", menuItem: [
-            MenuItem(price: 5.99, imageName: "cube", title: "Fries 1", description: "Good Choise to.."),
+        Menu(imageName: "friesLogo", title: "Fries", description: "Nice food", menuItem: [
+            MenuItem(price: 5.99, imageName: "fries1", title: "Fries 1", description: "Good Choise to.."),
             MenuItem(price: 6.99, imageName: "cube", title: "Fries 2", description: "Good Choise to.."),
             MenuItem(price: 7.99, imageName: "cube", title: "Fries 3", description: "Good Choise to.."),
             MenuItem(price: 8.99, imageName: "cube", title: "Fries 4", description: "Good Choise to.."),
             MenuItem(price: 9.99, imageName: "cube", title: "Fries 5", description: "Good Choise to..")
             ]),
-        Menu(imageName: "cube", title: "Drinks", description: "Nice food", menuItem: [
+        Menu(imageName: "drinkLogo", title: "Drinks", description: "Nice food", menuItem: [
             MenuItem(price: 3.99, imageName: "cube", title: "Drink 1", description: "Good Choise to.."),
             MenuItem(price: 5.99, imageName: "cube", title: "Drink 2", description: "Good Choise to.."),
             MenuItem(price: 4.99, imageName: "cube", title: "Drink 3", description: "Good Choise to.."),
             MenuItem(price: 7.99, imageName: "cube", title: "Drink 4", description: "Good Choise to.."),
             MenuItem(price: 2.99, imageName: "cube", title: "Drink 5", description: "Good Choise to..")
             ]),
-        Menu(imageName: "cube", title: "Souses", description: "Nice food", menuItem: [
+        Menu(imageName: "souseLogo", title: "Souses", description: "Nice food", menuItem: [
             MenuItem(price: 2.99, imageName: "cube", title: "Souse 1", description: "Good Choise to.."),
             MenuItem(price: 2.99, imageName: "cube", title: "Souse 2", description: "Good Choise to.."),
             MenuItem(price: 3.99, imageName: "cube", title: "Souse 3", description: "Good Choise to.."),
             MenuItem(price: 5.99, imageName: "cube", title: "Souse 4", description: "Good Choise to.."),
             MenuItem(price: 1.99, imageName: "cube", title: "Souse 5", description: "Good Choise to..")
-            ]),
-//        Menu(imageName: "cube", title: "Chicken", description: "Nice food", menuItem: [
-//            MenuItem(price: 8.99, imageName: "cube", title: "Chicken 1", description: "Good Choise to.."),
-//            MenuItem(price: 15.99, imageName: "cube", title: "Chicken 2", description: "Good Choise to.."),
-//            MenuItem(price: 11.99, imageName: "cube", title: "Chicken 3", description: "Good Choise to.."),
-//            MenuItem(price: 13.99, imageName: "cube", title: "Chicken 4", description: "Good Choise to.."),
-//            MenuItem(price: 19.99, imageName: "cube", title: "Chicken 5", description: "Good Choise to..")
-//            ])
+            ])
     ]
 }
--- OrderApp/Model/Sales.swift
+++ OrderApp/Model/Sales.swift
@@ -9,7 +9,8 @@ import Foundation
 
 
 struct Sale {
-    let imageName: String
+    let previewImageName: String
+    let backgroundImageName: String
     let title: String
     let description: String
 }
@@ -17,10 +18,10 @@ struct Sale {
 
 extension Sale {
     static let mockData: [Sale] = [
-    Sale(imageName: "cube", title: "Burger's waterfall", description: "Your chanse to..."),
-    Sale(imageName: "cube", title: "Happy Friday", description: "Your chanse to..."),
-    Sale(imageName: "cube", title: "First delivery sale", description: "Your chanse to..."),
-    Sale(imageName: "cube", title: "Hot Chicken Days", description: "Your chanse to..."),
-    Sale(imageName: "cube", title: "Strip for step", description: "Your chanse to...")
+        Sale(previewImageName: "burgersWaterfallPreview", backgroundImageName: "burgersWaterfallBackground", title: "Burger's waterfall", description: "Your chanse to..."),
+        Sale(previewImageName: "happyFridayPreview", backgroundImageName: "happyFridayBackground", title: "Happy Friday", description: "Your chanse to..."),
+        Sale(previewImageName: "firstDeliverySalePreview", backgroundImageName: "burgersWaterfallBackground", title: "First delivery sale", description: "Your chanse to..."),
+        Sale(previewImageName: "hotChickenDaysPreview", backgroundImageName: "hotChickenDaysBackground", title: "Hot Chicken Days", description: "Your chanse to..."),
+        Sale(previewImageName: "stripForStepPreview", backgroundImageName: "burgersWaterfallBackground", title: "Strip for step", description: "Your chanse to...")
     ]
 }
