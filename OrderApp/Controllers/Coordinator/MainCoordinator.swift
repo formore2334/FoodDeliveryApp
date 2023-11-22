b175b8b28a60de8c3c5967c02a0d44a70c6bf410
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-22 11:23:44 +0500

    Did change vc in MC to navVC's

--- OrderApp/Controllers/Coordinator/MainCoordinator.swift
+++ OrderApp/Controllers/Coordinator/MainCoordinator.swift
@@ -26,15 +26,24 @@ class MainCoordinator: Coordinator {
         menuVC.coordinator = self
         basketVC.coordinator = self
         
-        tabBarController.viewControllers = [homeVC, menuVC, basketVC]
+        // Embed controllers in a UINavigationController. Its doing to appear logoImage
+        let homeNavVC = UINavigationController(rootViewController: homeVC)
+        let menuNavVC = UINavigationController(rootViewController: menuVC)
+        //let basketNavVC = UINavigationController(rootViewController: basketVC)
+        navigationController.isNavigationBarHidden = true
+        
+        tabBarController.viewControllers = [homeNavVC, menuNavVC, basketVC]
         tabBarController.tabBar.tintColor = .black
         
-        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
-        menuVC.tabBarItem = UITabBarItem(title: "Menu", image: UIImage(systemName: "fork.knife"), tag: 1)
+        homeNavVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 0)
+        menuNavVC.tabBarItem = UITabBarItem(title: "Menu", image: UIImage(systemName: "fork.knife"), tag: 1)
         basketVC.tabBarItem = UITabBarItem(title: "Basket", image: UIImage(systemName: "basket"), tag: 2)
         
         navigationController.setViewControllers([tabBarController], animated: false)
     }
+        
+    
+
     
     func passOrderToBasket(menuItem: MenuItem) {
         if let basketVC = tabBarController.viewControllers?[2] as? BasketViewController {
