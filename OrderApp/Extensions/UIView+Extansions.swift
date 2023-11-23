00d7677922d265c7b207547f184846de1e483165
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-23 18:02:59 +0500

    Did change pin extensions

--- OrderApp/Extensions/UIView+Extansions.swift
+++ OrderApp/Extensions/UIView+Extansions.swift
@@ -12,28 +12,24 @@ extension UIView {
     
     func pin(to superView: UIView) {
         translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor),
-            leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
-            trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
-            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor)
+            heightAnchor.constraint(equalToConstant: 40.0),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 50),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -50),
+            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: -30)
         ])
+
     }
     
-}
-
-
-extension UIView {
-    
-    func pin(superView: UIView, top: CGFloat, leading: CGFloat, trailing: CGFloat, bottom: CGFloat) {
+    func pinPayBtn(to superView: UIView) {
         translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor, constant: top),
-            leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor, constant: leading),
-            trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor, constant: trailing),
-            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: bottom)
+            heightAnchor.constraint(equalToConstant: 40),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 60),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -60),
+            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor, constant: -30)
         ])
     }
     
