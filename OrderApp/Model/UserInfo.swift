6b78ace6efc089ab1284625a3c02d3bee0202ec0
Dzmitry Kalbasin <myattention15@gmail.com>
2023-11-29 20:29:27 +0500

    Change UserInfo model & enums

--- OrderApp/Model/UserInfo.swift
+++ OrderApp/Model/UserInfo.swift
@@ -8,17 +8,27 @@
 import Foundation
 
 
-enum CheckoutListInfo: String {
+enum CheckoutList: String {
     case name = "Name"
     case phone = "Phone Number"
+    case email = "Email"
     case address = "Address"
     case comment = "Comment"
     case coupon = "Coupon"
 }
 
-struct CheckoutList {
+enum CheckoutListError: String, CaseIterable, Error {
+    case name = "Name is invalid"
+    case phone = "Invalid phone number"
+    case email = "Missing @ char"
+    case address = "Address should be higher then 6 chars"
+    case coupon = "Coupon not valid"
+}
+
+struct UserInfo {
     var name: String
     var phone: String
+    var email: String
     var address: String
     var comment: String
     var coupon: String
