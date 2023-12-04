fd0513479ed4b815b3b099b40fe1920d262e7f37
Dzmitry Kalbasin <myattention15@gmail.com>
2023-12-04 14:45:41 +0500

    Add logic to SaleManager

--- OrderApp/Model/SalesManager/SalesManager.swift
+++ OrderApp/Model/SalesManager/SalesManager.swift
@@ -10,6 +10,20 @@ import Foundation
 
 struct SalesManager {
 
+    private let menu: [Menu] = Menu.mockData
     
+    let sales: [Sale] = Sale.mockData
+    
+    var menuItems: [MenuItem] = []
+    
+    mutating func extractMenuItems() {
+        let menuItems = self.menu.flatMap { $0.menuItem }
+        
+        self.menuItems = menuItems
+    }
+    
+    func getCurrentSale(with menuItem: MenuItem) -> Sale? {
+        return sales.first { $0.menuItems?.contains(menuItem) ?? false}
+    }
     
 }
