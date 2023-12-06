aa6c18cac6fa668a60bba8f1b9d1c03ccde1afeb
Dzmitry Kalbasin <myattention15@gmail.com>
2023-12-06 20:08:26 +0500

    Add loading view helper

---
+++ OrderApp/Controllers/Helpers/LoadingView.swift
@@ -0,0 +1,47 @@
+//
+//  LoadingView.swift
+//  OrderApp
+//
+//  Created by ForMore on 06/12/2023.
+//
+
+import UIKit
+
+
+class LoadingView: UIView {
+    
+    private lazy var activityIndicatorView: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = .gray
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup() {
+        addSubview(activityIndicatorView)
+        
+        NSLayoutConstraint.activate([
+            activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    func startAnimating() {
+        activityIndicatorView.startAnimating()
+    }
+
+    func stopAnimating() {
+        activityIndicatorView.stopAnimating()
+    }
+    
+}
