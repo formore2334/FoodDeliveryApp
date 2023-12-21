920cf4b08a3bbdc6dcf254ec88bb176edc353454
Dzmitry Kalbasin <myattention15@gmail.com>
2023-12-21 15:30:22 +0500

    Add function for network manager to fetch data

--- OrderApp/NetworkLayer/NetworkManager.swift
+++ OrderApp/NetworkLayer/NetworkManager.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class NetworkManager {
     
+    
     private func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
         guard
             let data = data,
@@ -22,6 +23,8 @@ class NetworkManager {
         return image
     }
     
+    
+    
     func fetchImage(url: URL, completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             let image = self?.handleResponse(data: data, response: response)
@@ -29,5 +32,21 @@ class NetworkManager {
         }
         .resume()
     }
+    
+    
+    
+    func fetchData(url: URL, completionHandler: @escaping (_ data: Data?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode <= 300 else {
+                return completionHandler(nil, error)
+            }
+            completionHandler(data, nil)
+        }
+        .resume()
+    }
+    
     
 }
