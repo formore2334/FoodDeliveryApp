204223c289d7d15ecec2604c47eeb3aac6636d54
Dzmitry Kalbasin <myattention15@gmail.com>
2023-12-21 15:32:33 +0500

    Added new sceneDelegate implementation

--- OrderApp/SceneDelegate.swift
+++ OrderApp/SceneDelegate.swift
@@ -7,32 +7,114 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+// Tracks loading error
+protocol DataDelegate: AnyObject {
+    func didReceiveValidData()
+    func didReceiveInvalidData()
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate, DataDelegate {
+    
     var coordinator: Coordinator?
     var window: UIWindow?
-
-
+    var tabBarController: UITabBarController?
+    var errorScreenViewController: ErrorScreenViewController?
+    var errorManager = ErrorManager()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-
         
-        let tabBarController = UITabBarController()
-        coordinator = MainCoordinator(tabBarController: tabBarController)
-        coordinator?.start()
+        errorManager.dataDelegate = self
         
-        let vc = PayViewController(userInfo: UserInfo(orderNumber: "", name: "Mike", phone: "123456789", email: "heloeverybodyimfine@gmail.com", address: "st. St. Patrick, b.74, 221", comment: "", coupon: ""), basket: Basket(basketItems: Basket.mockData.basketItems))
+        if errorManager.error != nil {
+            self.setErrorScreenViewController(to: window)
+        } else {
+            self.setTabBarController(to: window)
+        }
         
-        //let vc = CheckoutListViewController(basket: Basket(basketItems: Basket.mockData.basketItems))
-        let navVC = UINavigationController(rootViewController: vc)
-
-        window.rootViewController = tabBarController
+    }
+    
+    
+    func didReceiveValidData() {
+        guard let window = window else { return }
+        
+        UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: { [weak self] in
+            self?.setTabBarController(to: window)
+        }, completion: nil)
+        
+    }
+    
+    func didReceiveInvalidData() {
+        guard let window = window else { return }
+        
+        UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+            self?.setErrorScreenViewController(to: window)
+        }, completion: nil)
+        
+    }
+    
+    // Load main screen
+    private func setTabBarController(to window: UIWindow) {
+        if let tabBarController = self.tabBarController {
+            self.coordinator = MainCoordinator(tabBarController: tabBarController)
+            self.coordinator?.start()
+            window.rootViewController = tabBarController
+        } else {
+            let tabBarController = UITabBarController()
+            self.tabBarController = tabBarController
+            self.coordinator = MainCoordinator(tabBarController: tabBarController)
+            self.coordinator?.start()
+            window.rootViewController = tabBarController
+        }
+        self.errorScreenViewController = nil
+        
+        setWindow(window)
+    }
+    
+    // Load error screen
+    private func setErrorScreenViewController(to window: UIWindow) {
+        
+        if let errorScreenViewController = self.errorScreenViewController {
+            window.rootViewController = errorScreenViewController
+        } else {
+            let errorScreenViewController = ErrorScreenViewController()
+            self.errorScreenViewController = errorScreenViewController
+            window.rootViewController = errorScreenViewController
+        }
+        self.tabBarController = nil
+        
+        setWindow(window)
+    }
+    
+    // Makes main window visible
+    private func setWindow(_ window: UIWindow) {
         window.backgroundColor = .white
         window.makeKeyAndVisible()
         self.window = window
     }
+
+//        let window = UIWindow(windowScene: windowScene)
+//
+//
+//        let tabBarController = UITabBarController()
+//        coordinator = MainCoordinator(tabBarController: tabBarController)
+//        coordinator?.start()
+//
+////        let vc = PayViewController(userInfo: UserInfo(orderNumber: "", name: "Mike", phone: "123456789", email: "heloeverybodyimfine@gmail.com", address: "st. St. Patrick, b.74, 221", comment: "", coupon: ""), basket: Basket(basketItems: []))
+//
+//        let vc = ErrorScreenViewController()
+//
+//        //let vc = CheckoutListViewController(basket: Basket(basketItems: Basket.mockData.basketItems))
+//        let navVC = UINavigationController(rootViewController: vc)
+//
+//        window.rootViewController = navVC
+//        window.backgroundColor = .white
+//        window.makeKeyAndVisible()
+//        self.window = window
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
