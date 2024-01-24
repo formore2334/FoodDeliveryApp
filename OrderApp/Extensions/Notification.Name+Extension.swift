9c9410798ec359959268db478a87340a4e1661a2
Dzmitry Kalbasin <myattention15@gmail.com>
2024-01-24 18:22:45 +0500

    Add extension with notifications names

---
+++ OrderApp/Extensions/Notification.Name+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.Name+Extension.swift
+//  OrderApp
+//
+//  Created by ForMore on 24/01/2024.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static let backToHome = Notification.Name("BackToHomeNotification")
+}
