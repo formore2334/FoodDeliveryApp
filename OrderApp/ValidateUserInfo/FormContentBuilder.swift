3cebde12cb8d2e89000b8e0dd3fb507b9692dc83
Dzmitry Kalbasin <myattention15@gmail.com>
2024-01-29 16:15:46 +0500

    Did FormContentBuilder code refactoring

--- OrderApp/ValidateUserInfo/FormContentBuilder.swift
+++ OrderApp/ValidateUserInfo/FormContentBuilder.swift
@@ -16,11 +16,7 @@ class FormContentBuilder {
     
     let validateManager = ValidateManager()
     
-    var userInfo = UserInfo(orderNumber: "", name: "", phone: "", email: "", address: "", comment: "", coupon: "") {
-        didSet {
-           // print("DEBUG checkoutList: ", userInfo)
-        }
-    }
+    var userInfo = UserInfo(orderNumber: "", name: "", phone: "", email: "", address: "", comment: "", coupon: "")
     
     // Flag to shake button
     var isValid: Bool {
@@ -33,10 +29,16 @@ class FormContentBuilder {
     
     // Implementation to each case in CheckoutList form
     func updateUserInfo(text: String, for checkoutListItem: CheckoutList, with basket: Basket? = nil) -> (String, Any)? {
+        
         switch checkoutListItem {
+            
         case .name:
+            
+            // Validate incoming text
             switch validateManager.validateName(text) {
             case .success(let validText):
+                
+                // Change UserInfo model
                 userInfo.name = validText.capitalized
                 return (validText, "")
             case .failure(let errorText):
@@ -45,8 +47,12 @@ class FormContentBuilder {
             }
             
         case .phone:
+            
+            // Validate incoming text
             switch validateManager.validatePhoneNumber(text) {
             case .success(let validText):
+                
+                // Change UserInfo model
                 userInfo.phone = validText
                 return (validText, "")
             case .failure(let errorText):
@@ -55,17 +61,26 @@ class FormContentBuilder {
             }
             
         case .email:
+            
+            // Validate incoming text
             switch validateManager.validateEmail(text) {
             case .success(let validText):
+                
+                // Change UserInfo model
                 userInfo.email = validText
                 return (validText, "")
             case .failure(let errorText):
                 userInfo.email = ""
                 return ("", errorText.localizedDescription)
             }
+            
         case .address:
+            
+            // Validate incoming text
             switch validateManager.validateAddress(text) {
             case .success(let validText):
+                
+                // Change UserInfo model
                 userInfo.address = validText.capitalized
                 return (validText, "")
             case .failure(let errorText):
@@ -74,29 +89,40 @@ class FormContentBuilder {
             }
             
         case .comment:
+            
+            // Change UserInfo model
             userInfo.comment = text.capitalized
             return nil
             
         case .coupon:
+            
+            // Change UserInfo model
             userInfo.coupon = text
             
             guard let basket = basket else { return nil }
             
-            // Returned values for total label's
+            // Validate incoming coupon text
             couponsManager.basket = basket
             couponsManager.applyCoupon(text)
             
+            // Checks the returned value
             if couponsManager.couponIsValid {
                 
-                let calculatedDiscount = couponsManager.calculatedDiscountString
+                // Set needs total sum values
+                let newTotalSum = couponsManager.calculatedDiscountString
                 let totalSum = "\(basket.totalSum)"
-                let discount = totalSum.crossOutTheLine()
+                let oldTotalSum = totalSum.crossOutTheLine()
                 
-                return (calculatedDiscount, discount)
+                return (newTotalSum, oldTotalSum)
+            } else {
+                
+                // Clear UserInfo model coupon field
+                userInfo.coupon = ""
             }
+            
         }
-        return nil
         
+        return nil
     }
     
     
