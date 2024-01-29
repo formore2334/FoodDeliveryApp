2fc4019038c52f471b25f797165e2ca31eb0681f
Dzmitry Kalbasin <myattention15@gmail.com>
2024-01-29 16:44:03 +0500

    Add missing marks for extension & did small code refactoring

--- OrderApp/Extensions/String+CrossText.swift
+++ OrderApp/Extensions/String+CrossText.swift
@@ -7,14 +7,18 @@
 
 import UIKit
 
+// Extension with attributed string
 
-// Cross out the line for sales
 extension String {
     
+    // Cross out the string
     func crossOutTheLine() -> NSAttributedString {
         
         let attributedString = NSMutableAttributedString(string: self)
-        attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributedString.length))
+        
+        attributedString.addAttribute(NSAttributedString.Key.strikethroughStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSMakeRange(0, attributedString.length))
         
         return attributedString
     }
