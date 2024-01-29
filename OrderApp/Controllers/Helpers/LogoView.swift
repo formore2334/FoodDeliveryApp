00fcbc8966cca2c407e6eb24527e259c765687ce
Dzmitry Kalbasin <myattention15@gmail.com>
2024-01-29 16:07:35 +0500

    Did LogoView helper code refactoring

--- OrderApp/Controllers/Helpers/LogoView.swift
+++ OrderApp/Controllers/Helpers/LogoView.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 
-class LogoView: UIView {
+final class LogoView: UIView {
     
-    private let logoImageView: UIImageView = {
+    private lazy var logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logoWelms")
         imageView.contentMode = .scaleAspectFit
@@ -18,6 +18,8 @@ class LogoView: UIView {
         imageView.layer.masksToBounds = true
         return imageView
     }()
+    
+    // MARK: - Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,16 +29,23 @@ class LogoView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupNavigationBarLogo(in navigationController: UINavigationController, with navigationItem: UINavigationItem) {
+    // MARK: - Configurations
+    
+    func configureIntoNavBar(in navigationController: UINavigationController, with navigationItem: UINavigationItem) {
+        
+        // Config container for logo
         let logoContainer = UIView(frame: CGRect(x: 0, y: 0,
                                                  width: navigationController.navigationBar.bounds.width / 2,
                                                  height: navigationController.navigationBar.bounds.height))
         
         logoContainer.addSubview(logoImageView)
+        
+        // Config logo inside container
         logoImageView.frame = CGRect(x: logoContainer.center.x / 2, y: 0,
                                      width: navigationController.navigationBar.bounds.width / 4,
                                      height: navigationController.navigationBar.bounds.height * 0.8)
         
+        // Sets logo as titleView in nav bar pannel
         navigationItem.titleView = logoContainer
     }
 
