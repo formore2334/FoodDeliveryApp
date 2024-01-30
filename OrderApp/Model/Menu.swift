63bc5cc39db02e7190efb81375ca26bc4b16c2c7
Dzmitry Kalbasin <myattention15@gmail.com>
2024-01-30 19:13:16 +0500

    Add custom CodingKeys for models

--- OrderApp/Model/Menu.swift
+++ OrderApp/Model/Menu.swift
@@ -60,3 +60,70 @@ struct SpecialMenuItem: MenuItemProtocol, Discountable {
     var description: String
     var sale: Int
 }
+
+// Creates custom CodingKeys to hanling formatting characters \t, \n from JSON
+
+extension RegularMenuItem {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, price, imageURL, title, description
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        price = try container.decode(Double.self, forKey: .price)
+        imageURL = try container.decode(String.self, forKey: .imageURL)
+        title = try container.decode(String.self, forKey: .title)
+        
+        // Replace special formatting characters
+        let rawDescription = try container.decode(String.self, forKey: .description)
+        description = rawDescription.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
+    }
+    
+}
+
+extension DiscountMenuItem {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, price, imageURL, title, description, sale
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        price = try container.decode(Double.self, forKey: .price)
+        imageURL = try container.decode(String.self, forKey: .imageURL)
+        title = try container.decode(String.self, forKey: .title)
+        sale = try container.decode(Int.self, forKey: .sale)
+        
+        // Replace special formatting characters
+        let rawDescription = try container.decode(String.self, forKey: .description)
+        description = rawDescription.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
+    }
+    
+}
+
+extension SpecialMenuItem {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, price, imageURL, title, description, sale
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        price = try container.decode(Double.self, forKey: .price)
+        imageURL = try container.decode(String.self, forKey: .imageURL)
+        title = try container.decode(String.self, forKey: .title)
+        sale = try container.decode(Int.self, forKey: .sale)
+        
+        // Replace special formatting characters
+        let rawDescription = try container.decode(String.self, forKey: .description)
+        description = rawDescription.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
+    }
+    
+}
--- OrderApp/Model/Sales.swift
+++ OrderApp/Model/Sales.swift
@@ -13,7 +13,37 @@ struct Sale: Identifiable, Decodable {
     let previewImageURL: String
     let backgroundImageURL: String
     let title: String
-    let textHeadline: String
-    let textDescription: String
+    var textHeadline: String
+    var textDescription: String
     var menuItemsID: [Int]? = nil
+}
+
+// Creates custom CodingKeys to hanling formatting characters \t, \n from JSON
+
+extension Sale {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, previewImageURL, backgroundImageURL, title, textHeadline, textDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        previewImageURL = try container.decode(String.self, forKey: .previewImageURL)
+        backgroundImageURL = try container.decode(String.self, forKey: .backgroundImageURL)
+        title = try container.decode(String.self, forKey: .title)
+        textHeadline = try container.decode(String.self, forKey: .textHeadline)
+        textDescription = try container.decode(String.self, forKey: .textDescription)
+        
+        // Replace special formatting characters
+        
+        let rawTextHeadline = try container.decode(String.self, forKey: .textHeadline)
+        textHeadline = rawTextHeadline.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
+        
+        let rawTextDescription = try container.decode(String.self, forKey: .textDescription)
+        textDescription = rawTextDescription.replacingOccurrences(of: "\\n", with: "\n")
+            .replacingOccurrences(of: "\\t", with: "\t")
+    }
+    
 }
